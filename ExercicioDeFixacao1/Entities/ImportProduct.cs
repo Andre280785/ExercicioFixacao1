439647a439efe3547594d5f52eb0dae4ecc747c4
André Tavares Machado <andre266@outlook.com.br>
2020-02-21 13:46:43 -0300

    Exercício de Fixação Herança e Polimorfismo

---
+++ ExercicioDeFixacao1/Entities/ImportProduct.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace ExercicioDeFixacao1.Entities
+{
+    class ImportProduct : Product
+    {
+        public double customsFee { get; set; }
+
+        public ImportProduct()
+        {
+        }
+
+        public ImportProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            this.customsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + customsFee;
+        } 
+        public override string PriceTag()
+        {
+            return Name + " $ " + TotalPrice().ToString("F2",CultureInfo.InvariantCulture) + $"(Customs fee: $ {customsFee.ToString("F2",CultureInfo.InvariantCulture)})";
+        }
+    }
+}
---
+++ ExercicioDeFixacao1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ExercicioDeFixacao1.Entities;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioDeFixacao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Product> list = new List<Product>();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char resp = char.Parse(Console.ReadLine());
+                if ( resp == 'c')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                } else if (resp == 'u')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                }
+            }
+        }
+    }
+}
